97eb291d19c9e7cf6b3bc97c7cfc7f8f0992a70f
Seoyeong <gjtjdud93@gmail.com>
2017-07-26 03:11:46 +0900

    6. 모두를 위한 평등 (TestCode)

--- Tests/TDD_SwiftTests/TDD_SwiftTests.swift
+++ Tests/TDD_SwiftTests/TDD_SwiftTests.swift
@@ -12,12 +12,14 @@ class TDD_SwiftTests: XCTestCase {
     func testEquality() {
         XCTAssertTrue(Dollor(amount: 5) == Dollor(amount: 5))
         XCTAssertFalse(Dollor(amount: 5) == Dollor(amount: 6))
+        XCTAssertTrue(Franc(amount: 5) == Franc(amount: 5))
+        XCTAssertFalse(Franc(amount: 5) == Franc(amount: 6))
     }
     
     func testFrancMultiplication() {
         let five: Franc = Franc(amount: 5)
         XCTAssertEqual(Franc(amount: 10), five.times(multiplier: 2))
         XCTAssertEqual(Franc(amount: 15), five.times(multiplier: 3))
-        
     }
+    
 }
