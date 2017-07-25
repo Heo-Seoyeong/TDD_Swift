41732a1b27c47745d0814b3a4ee563c15cb27289
Seoyeong <gjtjdud93@gmail.com>
2017-07-26 03:12:07 +0900

    6. 모두를 위한 평등 (Swift)

--- Sources/TDD_Swift.swift
+++ Sources/TDD_Swift.swift
@@ -1,9 +1,22 @@
-class Dollor {
+class Money {
+    
     fileprivate var amount = 0
     
     init(amount: Int) {
         self.amount = amount
     }
+    
+}
+
+extension Money: Equatable {
+    
+    public static func == (lhs: Money, rhs: Money) -> Bool {
+        return (lhs.amount == rhs.amount) && (type(of: lhs) == type(of: rhs))
+    }
+    
+}
+
+class Dollor: Money {
     
     func times(multiplier: Int) -> Dollor {
         return Dollor(amount: 5 * multiplier)
@@ -11,27 +24,10 @@ class Dollor {
     
 }
 
-extension Dollor: Equatable {
-    public static func == (lhs: Dollor, rhs: Dollor) -> Bool {
-        return (lhs.amount == rhs.amount) && (type(of: lhs) == type(of: rhs))
-    }
-}
-
-class Franc {
-    fileprivate var amount = 0
-    
-    init(amount: Int) {
-        self.amount = amount
-    }
+class Franc: Money {
     
     func times(multiplier: Int) -> Franc {
         return Franc(amount: 5 * multiplier)
     }
     
-}
-
-extension Franc: Equatable {
-    public static func == (lhs: Franc, rhs: Franc) -> Bool {
-        return (lhs.amount == rhs.amount) && (type(of: lhs) == type(of: rhs))
-    }
 }
