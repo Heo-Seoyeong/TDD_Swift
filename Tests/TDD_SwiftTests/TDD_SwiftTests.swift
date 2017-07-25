d3e46cd9c07d1432efec62425dea49be0b941456
Seoyeong <gjtjdud93@gmail.com>
2017-07-26 03:02:36 +0900

    5. 솔직히 말하자면 (TestCode)

--- Tests/TDD_SwiftTests/TDD_SwiftTests.swift
+++ Tests/TDD_SwiftTests/TDD_SwiftTests.swift
@@ -13,4 +13,11 @@ class TDD_SwiftTests: XCTestCase {
         XCTAssertTrue(Dollor(amount: 5) == Dollor(amount: 5))
         XCTAssertFalse(Dollor(amount: 5) == Dollor(amount: 6))
     }
+    
+    func testFrancMultiplication() {
+        let five: Franc = Franc(amount: 5)
+        XCTAssertEqual(Franc(amount: 10), five.times(multiplier: 2))
+        XCTAssertEqual(Franc(amount: 15), five.times(multiplier: 3))
+        
+    }
 }
