5d322f3ee0a3c17d4d64fcad3a5cfa20f2715fa9
Seoyeong <gjtjdud93@gmail.com>
2017-08-25 17:37:16 +0900

    16. 드디어 추상화 (Swift)

--- Sources/TDD_Swift.swift
+++ Sources/TDD_Swift.swift
@@ -94,16 +94,19 @@ extension Money: Equatable {
 struct Pair {
     let from: String
     let to: String
+    
+    init(from: String, to: String) {
+        self.from = from
+        self.to = to
+    }
+    
+    var hashValue: Int = 0
 }
 
 extension Pair: Hashable {
     
     public static func ==(lhs: Pair, rhs: Pair) -> Bool{
         return lhs.from == rhs.from && lhs.to == rhs.to
-    }
-    
-    var hashValue: Int {
-        return 0
     }
     
 }
