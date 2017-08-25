d8c7df512e441229e46a220043c7943c7818c969
Seoyeong <gjtjdud93@gmail.com>
2017-08-25 15:20:02 +0900

    11. 모든 악의 근원 (Swift)

--- Sources/TDD_Swift.swift
+++ Sources/TDD_Swift.swift
@@ -12,12 +12,12 @@ class Money {
         return Money(amount: amount * multiplier, currency: currency)
     }
     
-    class func dollar(amount: Int) -> Dollar {
-        return Dollar(amount: amount, currency: "USD")
+    class func dollar(amount: Int) -> Money {
+        return Money(amount: amount, currency: "USD")
     }
     
-    class func franc(amount: Int) -> Franc {
-        return Franc(amount: amount, currency: "CHF")
+    class func franc(amount: Int) -> Money {
+        return Money(amount: amount, currency: "CHF")
     }
     
 }
@@ -26,22 +26,6 @@ extension Money: Equatable {
     
     public static func == (lhs: Money, rhs: Money) -> Bool {
         return (lhs.amount == rhs.amount) && (lhs.currency == rhs.currency)
-    }
-    
-}
-
-class Dollar: Money {
-    
-    override func times(multiplier: Int) -> Money {
-        return Money(amount: amount * multiplier, currency: currency)
-    }
-    
-}
-
-class Franc: Money {
-    
-    override func times(multiplier: Int) -> Money {
-        return Money(amount: amount * multiplier, currency: currency)
     }
     
 }
